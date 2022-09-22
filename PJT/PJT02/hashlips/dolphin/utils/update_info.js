4aa29e014ed651b762c5af2eb599879d07940566
Youji-Sung <anc9432@gmail.com>
2022-09-22 22:52:40 +0900

    Make dolphin generative art

---
+++ PJT/PJT02/hashlips/dolphin/utils/update_info.js
@@ -0,0 +1,34 @@
+const basePath = process.cwd();
+const { NETWORK } = require(`${basePath}/constants/network.js`);
+const fs = require("fs");
+
+const { baseUri, description, namePrefix, network, solanaMetadata } = require(`${basePath}/src/config.js`);
+
+// read json data
+let rawdata = fs.readFileSync(`${basePath}/build/json/_metadata.json`);
+let data = JSON.parse(rawdata);
+
+data.forEach((item) => {
+  if (network == NETWORK.sol) {
+    item.properties.name.description = `${namePrefix} #${item.properties.edition}`;
+    item.properties.description.description = description;
+    item.creators = solanaMetadata.creators;
+  } else {
+    item.properties.name.description = `${namePrefix} #${item.properties.edition}`;
+    item.properties.description.description = description;
+    item.properties.image.description = `${baseUri}/${item.properties.edition}.png`;
+  }
+  fs.writeFileSync(`${basePath}/build/json/${item.properties.edition}.json`, JSON.stringify(item, null, 2));
+});
+
+fs.writeFileSync(`${basePath}/build/json/_metadata.json`, JSON.stringify(data, null, 2));
+
+if (network == NETWORK.sol) {
+  console.log(`Updated description for images to ===> ${description}`);
+  console.log(`Updated name prefix for images to ===> ${namePrefix}`);
+  console.log(`Updated creators for images to ===> ${JSON.stringify(solanaMetadata.creators)}`);
+} else {
+  console.log(`Updated baseUri for images to ===> ${baseUri}`);
+  console.log(`Updated description for images to ===> ${description}`);
+  console.log(`Updated name prefix for images to ===> ${namePrefix}`);
+}
